31b8c1befee4038e9dfc3b181ee4d75e3ab57f81
ammar <ammar@gmail.com>
2024-07-13 03:47:36 +0300

    cat

---
+++ src/assets/3d-models/index.js
@@ -0,0 +1,24 @@
+import carScene from "./car.glb"
+import catScene from "./cat.glb"
+import ponyScene from "./pony.glb"
+import skyScene from "./sky.glb"
+import sky2Scene from "./sky2.glb"
+import robotScene from "./robot.glb"
+import islandScene from "./island.glb"
+import ballScene from "./ball.glb"
+import moonScene from "./moon.glb"
+import cityScene from "./city.glb"
+import nightScene from "./night.glb"
+import planetScene from "./planet.glb"
+import robotArmScene from "./robotArm.glb"
+
+
+export {
+    carScene, catScene,
+    ponyScene, skyScene,
+    sky2Scene, robotScene,
+    islandScene, ballScene,
+    moonScene, cityScene,
+    nightScene, planetScene,
+    robotArmScene
+}
